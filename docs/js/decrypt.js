2d70bb5f47f7ffa16cd7c8b544d507362c0300e3
Ashrit Yarava <ashdragon95@gmail.com>
2020-08-24 19:47:52 -0400

    Decryption works as well.

--- docs/js/decrypt.js
+++ docs/js/decrypt.js
@@ -1,3 +1,48 @@
-function decrypt() {
-    
+function getDecryptTable() {
+    let emojis = "😀😃😄😁😆😅🤣😂🙂🙃😉😊😇🥰😍🤩😘😗😚😙😋😛😜🤪😝🤑🤗🤭🤫🤔🤐🤨😐😑😶😏😒🙄😬🤥😌😔😪🤤😴😷🤒🤕🤢🤮🤧🥵🥶🥴😵🤯🤠🥳😎🤓🧐😕😟🙁😮😯😲😳🥺😦😧😨😰😥😢😭😱😖😣😞😓😩😫🥱😤😡😠🤬😈👿💀💩🤡👹👺👻👽👾🤖😺😸😹😻😼😽🙀😿😾💋👋🤚🖐✋🖖👌🤏🤞🤟🤘🤙👈👉👆🖕👇👍👎✊"
+    let e2i = new Map()
+    let index = 0
+    for(let emoji of emojis) {
+        e2i.set(emoji, index)
+        index++
+    }
+    return e2i
+}
+
+function getBin(number) {
+    let bin = number.toString(2)
+    while(bin.length < 7) {
+        bin = "0" + bin
+    }
+    return bin
+}
+
+function split(binaryStream) {
+    let bins = []
+    let current = ""
+    for(let bin of binaryStream) {
+        if(current.length < 8) {
+            current += bin
+        } else {
+            bins.push(current)
+            current = bin
+        }
+    }
+    return bins
+}
+
+function decrypt(message) {
+    let e2i = getDecryptTable()
+    let binaryStream = ""
+    for(let emoji of message) {
+        let number = e2i.get(emoji)
+        binaryStream += getBin(number)
+    }
+    let bytes = split(binaryStream)
+    let text = ""
+    for(let byte of bytes) {
+        number = parseInt(byte, 2)
+        text += String.fromCharCode(number)
+    }
+    return text
 }
--- docs/js/encrypt.js
+++ docs/js/encrypt.js
@@ -56,5 +56,3 @@ function encrypt(message) {
     }
     return emojis
 }
-
-console.log(encrypt("Hello!"))
