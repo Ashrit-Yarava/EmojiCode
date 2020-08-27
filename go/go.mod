723f9a98d83fc7c95b18e9d1e9aeb95e93068dcf
Ashrit Yarava <ashdragon95@gmail.com>
2020-08-27 10:32:48 -0400

    Clean up workspace.

--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Ashrit-Yarava/emojicode
-
-go 1.15
--- scrapbook/test_string.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-message=$(python3 encrypt.py "$1" | xargs python3 decrypt.py)
-if [ "$message" != "$1" ]
-then
-    echo "$1 does not work..."
-else
-    echo "Works!"
-fi
